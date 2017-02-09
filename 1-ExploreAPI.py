b044d6d657e32daec7b8ac8199f452d305d07f4d
Александр Платонов < >
2017-02-09 14:25:10 +0300

    Move functions to lib file

--- 1-ExploreAPI.py
+++ 1-ExploreAPI.py
@@ -1,30 +1,8 @@
-import urllib.request
-import urllib.parse
-import json
+from lib import make_tmdb_api_request
 
 #number of the movie to find
 movie_number = str(215)
 
-#get api key
-with open("apikey.json") as file_apikey:
-    api_key = json.load(file_apikey)['key']
-
-def load_json_data_from_url(base_url, url_params):
-    url = '%s?%s' % (base_url, urllib.parse.urlencode(url_params))
-    response = urllib.request.urlopen(url).read().decode('utf-8')
-    return json.loads(response)
-
-
-def make_tmdb_api_request(method, api_key, extra_params=None):
-    extra_params = extra_params or {}
-    url = 'https://api.themoviedb.org/3%s' % method
-    params = {
-        'api_key': api_key,
-        'language': 'ru',
-    }
-    params.update(extra_params)
-    return load_json_data_from_url(url, params)
-
-movie_info = make_tmdb_api_request(method='/movie/' + movie_number, api_key=api_key)
+movie_info = make_tmdb_api_request(method='/movie/' + movie_number)
 print(movie_info['budget'])
 
---
+++ lib.py
@@ -0,0 +1,23 @@
+import urllib.request
+import urllib.parse
+import json
+
+#get api key
+with open("apikey.json") as file_apikey:
+    api_key = json.load(file_apikey)['key']
+
+def load_json_data_from_url(base_url, url_params):
+    url = '%s?%s' % (base_url, urllib.parse.urlencode(url_params))
+    response = urllib.request.urlopen(url).read().decode('utf-8')
+    return json.loads(response)
+
+
+def make_tmdb_api_request(method, extra_params=None):
+    extra_params = extra_params or {}
+    url = 'https://api.themoviedb.org/3%s' % method
+    params = {
+        'api_key': api_key,
+        'language': 'ru',
+    }
+    params.update(extra_params)
+    return load_json_data_from_url(url, params)
